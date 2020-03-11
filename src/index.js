0cf0ebe579d1518a2d40a39332e9e16675685ae9
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-03-10 20:35:39 -0500

    first commit per today

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.set('port', process.env.PORT || 3001);
+
+app.use(express.json());
+app.use(cors());
+
+app.use(require('./routes/index'));
+
+app.get('/', (req,res) => {
+    res.json({
+        status: 'works'
+    })
+})
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+})
---
+++ src/routes/index.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const shoppingService = require('../services/products');
+
+
+router.get('/api/all_products_purchase', async (req,res) => {
+    try {
+        let response = await shoppingService.getAllProducts();
+        res.json({
+            products: response
+        })
+        .status(200)
+    } catch (error) {
+        res.json({
+            message: "failed response"
+        })
+        status(500)
+    }
+})
+
+router.get('/api/products_by_id/:id', async (req,res) => {
+    let id = req.params.id
+    try {
+        let response = await shoppingService.getProductById(id);
+        res.json({
+             product: response 
+        })
+        .status(200)
+    } catch (err) {
+        res.json({
+            message: "failed response"
+        })
+        status(500)
+    }
+})
+
+router.get('/api/get_orders_by_id/:id', async (req,res) => {
+    let id = req.params.id
+    try{
+        let response = await shoppingService.getOrdersById(id);
+        res.json({
+            orders: response 
+        })
+        .status(200)
+    } catch (err) {
+        res.json({
+            message: "failed petition"
+        })
+        .status(500)
+    }
+})
+
+router.get('/api/product_detail/:id', async (req,res) => {
+    let id = req.params.id
+    try {
+        let response = await shoppingService.getProductDetailByOrderId(id);
+        res.json({
+            product: response
+        })
+        .status(200)
+    } catch (error) {
+        res.json({
+            message: "failed petition"
+        })
+        .status(500)
+    }
+})
+
+
+
+module.exports = router;
